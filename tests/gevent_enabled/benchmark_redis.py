7577a72200cfc09a1fcef08f388da580be762eba
OK-UNDP <OK-UNDP@users.noreply.github.com>
2019-09-06 23:55:24 -0400

    fix: test: Gevent monkey patching (fixes #6).
    Ensure `gevent` monkey patching is performed
    only for tests requiring it.

--- tests/gevent_enabled/benchmark_redis.py
+++ tests/gevent_enabled/benchmark_redis.py
@@ -12,11 +12,23 @@ class TestBenchmark:
     """Performance test suite using MS Graph API task base class."""
 
     @pytest.mark.parametrize("target_suffix", ["multi_spawn", "multi_imap", "single"])
-    @pytest.mark.parametrize("push_mode", ["pipe"])  # "naive"
+    @pytest.mark.parametrize(
+        "push_mode", ["pipe"]  # add "naive" push_mode for full coverage (slow!)
+    )
     def test_redis_push(
         self, benchmark, graph_api_task, logger, push_mode, target_suffix, test_data
     ):
         """Benchmark routine to evaluate pushing records to Redis."""
+        import gevent.socket
+        import socket
+
+        logger.info(
+            "Gevent monkey patching is %s",
+            "ENABLED" if socket.socket is gevent.socket.socket else "DISABLED",
+        )
+
+        assert socket.socket is gevent.socket.socket  # noqa
+
         logger.info("Start benchmark")
 
         result = benchmark.pedantic(
